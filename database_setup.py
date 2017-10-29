cc89867d30c9e556a80970acb988972f61bc8e65
unknown <sebas_bordolino@hotmail.com>
2017-10-29 15:17:23 -0300

    programa con login local y algunas restricciones

---
+++ database_setup.py
@@ -0,0 +1,29 @@
+import sys
+from sqlalchemy import Column, ForeignKey, Integer,String,Date
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from sqlalchemy import create_engine
+import datetime
+
+Base=declarative_base()
+
+class Blog(Base):
+	__tablename__='blog'
+	
+	id=Column(Integer,primary_key=True)
+	titulo=Column(String(50),nullable=False)
+	contenido=Column(String(280),nullable=False)
+
+	
+	
+class User(Base):
+	__tablename__='user'
+	
+	id=Column(Integer,primary_key=True)
+	username=Column(String(50),nullable=False)
+	password=Column(String(250),nullable=False)
+	email=Column(String(250),nullable=False)
+
+engine=create_engine('sqlite:///blog.db')
+
+Base.metadata.create_all(engine)	
